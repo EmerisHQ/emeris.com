f357eb26367d839be6a45d9d386d5e9540507a24
Cyrus Goh <hello@lovincyrus.com>
2021-07-14 14:22:17 -0700

    wip section-cta

--- components/SectionCta.vue
+++ components/SectionCta.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="tm-container tm-wrapper tm-section center">
-    <h1>Section Cta</h1>
+  <div class="section-cta tm-container tm-wrapper tm-section">
+    <!-- graphics -->
+    <div class="tm-content center text">
+      <h1 class="tm-rf5 tm-bold tm-lh-title tm-serif">
+        Experience a glimpse<br />of DeFi's future<br />today
+      </h1>
+      <tm-button
+        variant="text"
+        to-link="external"
+        href="https://medium.com/emeris-blog/introducing-emeris-the-cross-chain-portal-to-all-crypto-apps-4e6eee5b53a8"
+        size="l"
+        class="mt-6"
+        >Get started <span class="icon__right">&#8594;</span></tm-button
+      >
+    </div>
   </div>
 </template>
 
@@ -8,4 +21,11 @@
 export default {}
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.section-cta
+  padding var(--spacing-11) 0
+
+.text
+  text-align center
+  padding 0 3.5313rem
+</style>
