45b6ae4572e418e25402834d4227b34d8bbda576
Cyrus Goh <hello@lovincyrus.com>
2021-07-16 13:05:09 -0700

    wip section hero, tweak nav

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,13 +1,23 @@
 <template>
-  <div
-    id="homepage"
-    class="section-hero tm-section tm-container tm-wrapper center"
-  >
-    <h1 class="tm-serif tm-rf4 tm-bold tm-lh-title">
-      <span class="left">Experience</span
-      ><span class="center">the power of</span
-      ><span class="right">cross-chain DeFi</span>
-    </h1>
+  <div id="homepage" class="section-hero tm-container tm-wrapper">
+    <div class="outer tm-container-narrow mt-5">
+      <div class="inner center">
+        <h1
+          class="title tm-content tm-serif tm-rf5 tm-bold tm-lh-title tm-title"
+        >
+          <span class="tm-text-left">Experience</span>
+          <span class="tm-text-center">the power of</span>
+          <span class="tm-text-right">cross-chain DeFi</span>
+        </h1>
+      </div>
+      <tm-button
+        variant="text"
+        to-link="external"
+        href="https://medium.com/emeris-blog/introducing-emeris-the-cross-chain-portal-to-all-crypto-apps-4e6eee5b53a8"
+        class="btn"
+        >Launch app <span class="icon__right">&#8594;</span></tm-button
+      >
+    </div>
   </div>
 </template>
 
@@ -16,12 +26,26 @@ export default {}
 </script>
 
 <style lang="stylus" scoped>
-.left
-  text-align left
+.section-hero
+.outer
+.inner
+  position relative
 
-.center
-  text-align center
+.inner
+  padding-top 81.5px
+  padding-bottom 395.5px
+  padding-left 112px
+  padding-right 112px
 
-.right
-  text-align right
+a.btn
+  position absolute
+  bottom 0
+  right 0
+
+.section-hero
+  padding-top var(--spacing-5)
+  padding-bottom var(--spacing-10)
+
+.title span
+  display block
 </style>
