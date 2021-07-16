7e32f7c21546a6389775243908a74fe8e13c7eba
Cyrus Goh <hello@lovincyrus.com>
2021-07-15 19:18:10 -0700

    wip section-access, logos orbs

--- components/SectionAccess.vue
+++ components/SectionAccess.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="section-access tm-section">
+    <div class="tm-container">
+      <img class="ring z-0" src="~/assets/images/elements/ring.png" alt="" />
+    </div>
     <div class="tm-wrapper tm-content">
-      <div class="top">
-        <h1 class="tm-rf5 tm-bold tm-lh-title tm-serif">
-          Early access to innovative blockchains
-        </h1>
+      <h1 class="title tm-rf5 tm-bold tm-lh-title tm-serif">
+        Early access to innovative blockchains
+      </h1>
+    </div>
+    <div class="tm-container-wide logos-container mt-9 z-2">
+      <div class="scrolling-wrapper">
+        <tm-link v-for="item in list" :key="item.text" :href="item.url">
+          <!-- <tm-tooltip :text="`${item.text} ↗`" position="top-center"> -->
+          <img
+            :src="require(`~/assets/images/logos/${item.img}.svg`)"
+            :alt="item.text"
+            class="logo-item"
+          />
+          <!-- </tm-tooltip> -->
+        </tm-link>
       </div>
-      <div class="bottom tm-grid-base">
+    </div>
+    <div class="bottom tm-wrapper tm-content">
+      <div class="tm-grid-base">
         <div class="left">
           Access cutting-edge decentralized exchanges, allowing you to obtain
           experimental or alternative coins that may be difficult or impossible
@@ -20,46 +36,6 @@
         </div>
       </div>
     </div>
-    <div
-      class="content mt-6"
-      :class="{ paused: hover }"
-      @mouseover="hover = true"
-      @mouseleave="hover = false"
-    >
-      <div class="text">
-        <tm-link v-for="item in list" :key="item.text" :href="item.url">
-          <tm-tooltip :text="`${item.text} ↗`" position="top-center">
-            <img
-              :src="require(`~/assets/images/logos/${item.img}.svg`)"
-              :alt="item.text"
-              class="logo-item"
-            />
-          </tm-tooltip>
-        </tm-link>
-      </div>
-      <div class="text">
-        <tm-link v-for="item in list" :key="item.text" :href="item.url">
-          <tm-tooltip :text="`${item.text} ↗`" position="top-center">
-            <img
-              :src="require(`~/assets/images/logos/${item.img}.svg`)"
-              :alt="item.text"
-              class="logo-item"
-            />
-          </tm-tooltip>
-        </tm-link>
-      </div>
-      <div class="text">
-        <tm-link v-for="item in list" :key="item.text" :href="item.url">
-          <tm-tooltip :text="`${item.text} ↗`" position="top-center">
-            <img
-              :src="require(`~/assets/images/logos/${item.img}.svg`)"
-              :alt="item.text"
-              class="logo-item"
-            />
-          </tm-tooltip>
-        </tm-link>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -67,7 +43,6 @@
 export default {
   data() {
     return {
-      hover: false,
       list: [
         {
           url: 'https://akash.network',
@@ -104,6 +79,11 @@ export default {
           text: 'Persistence',
           img: 'persistence',
         },
+        {
+          url: 'https://cosmos.network',
+          text: 'Cosmos Hub',
+          img: 'cosmos',
+        },
       ],
     }
   },
@@ -111,47 +91,37 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-// .top
-.bottom .left
-.bottom .right
-  grid-column 1/-1
-
-.top
-  max-width 50.0625rem
-  text-align center
-  grid-row 1
+.logos-container
+  position relative
 
 .tm-link
   color var(--secondary)
 
-.wrap
-  overflow hidden
+.bottom .left
+.bottom .right
+  grid-column 1/-1
 
-.content
-  width 6250rem
+.title
+  text-align center
 
-.text
-  animation-name animation
-  animation-timing-function linear
-  animation-iteration-count infinite
-  animation-duration 30s
-  float left
+.ring
+  position absolute
 
-.text > a
-  opacity 0.35
-  margin-left 1.25rem
+.scrolling-wrapper
+  overflow-x scroll
+  overflow-y hidden
+  white-space nowrap
+  -webkit-overflow-scrolling touch
 
-.paused .text
-  animation-play-state paused
+  &::-webkit-scrollbar
+    display none
 
 .logo-item
-  height var(--spacing-9)
+  display inline-block
+  height 16.9375rem
 
-@keyframes animation
-  0%
-    transform translateX(0)
-  100%
-    transform translateX(-100%)
+.text > a
+  margin-left 1.25rem
 
 @media $breakpoint-xsmall-only
   .bottom .right
