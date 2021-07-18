63b94e4203213d3117d4bb2474ebc094c509d1a8
Daria K <ppartesia@gmail.com>
2021-07-19 01:17:10 +0500

    add protocol section

--- components/SectionProtocol.vue
+++ components/SectionProtocol.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="center">
-    <h1>Section Protocol</h1>
+  <div class="section-protocol center">
+    <div class="hand-top">
+      <img
+        class="hand-top-img"
+        src="~/assets/images/elements/hand.png"
+        alt=""
+      />
+      <div class="hand-top-circle">
+        <img src="~/assets/images/elements/circle-top.svg" />
+      </div>
+    </div>
+    <img
+      class="logo"
+      src="~/assets/images/logos/gravity.svg"
+      alt="Gravity DEX protocol"
+    />
+    <div class="hand-bottom">
+      <img
+        class="hand-bottom-img"
+        src="~/assets/images/elements/hand.png"
+        alt=""
+      />
+      <div class="hand-bottom-circle">
+        <img src="~/assets/images/elements/circle-bottom.svg" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +32,124 @@
 export default {}
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.section-protocol
+  position relative
+  margin-bottom var(--spacing-12)
+  padding-top var(--spacing-12)
+  padding-bottom var(--spacing-12)
+  @media $breakpoint-medium
+    padding-top var(--spacing-10)
+    padding-bottom var(--spacing-10)
+
+.logo
+  width 100%
+  max-width 7.5rem
+  @media $breakpoint-medium
+    max-width 185px
+  @media $breakpoint-xl
+    max-width 173px
+
+.hand-top
+  position absolute
+  top -4.8rem
+  left calc(50% + 7.15rem)
+  @media $breakpoint-medium
+    top -12.05rem
+    left calc(50% + 11.22rem)
+  @media $breakpoint-xl
+    top -20.4rem
+    left calc(50% + 17.35rem)
+  &-img
+    mix-blend-mode hard-light
+    width 11.75rem
+    max-width none
+    transform rotate(-107.89deg)
+    @media $breakpoint-medium
+      width 19.2rem
+    @media $breakpoint-xl
+      width 29.3rem
+  &-circle
+    position absolute
+    width 4.5375rem
+    height 4.5375rem
+    top 44%
+    left -18%
+    transform rotate(-90deg)
+    @media $breakpoint-medium
+      width 7.21125rem
+      height 7.21125rem
+    @media $breakpoint-xl
+      width 11.769375rem
+      height 11.769375rem
+    &:before
+      content ''
+      opacity 0.28
+      position absolute
+      width 12.044375rem
+      height 12.044375rem
+      top 50%
+      left 50%
+      mix-blend-mode hard-light
+      background radial-gradient(50% 50% at 50% 50%, #FFFE2E 43.09%, rgba(117, 255, 189, 0) 100%)
+      transform translate(-50%, -50%)
+      @media $breakpoint-medium
+        width 15.625rem
+        height 15.625rem
+      @media $breakpoint-xl
+        width 25.5rem
+        height 25.5rem
+    img
+      width 100%
+
+.hand-bottom
+  position absolute
+  bottom -7.5rem
+  right calc(50% + 6.2rem)
+  @media $breakpoint-medium
+    bottom -15.25rem
+    right calc(50% + 10.4rem)
+  @media $breakpoint-xl
+    bottom -24.7rem
+    right calc(50% + 15.8rem)
+  &-img
+    mix-blend-mode hard-light
+    width 11.75rem
+    max-width none
+    transform rotate(75deg)
+    @media $breakpoint-medium
+      width 19.2rem
+    @media $breakpoint-xl
+      width 29.3rem
+  &-circle
+    position absolute
+    width 4.73125rem
+    height 4.73125rem
+    top 28%
+    right -20%
+    @media $breakpoint-medium
+      width 7.21125rem
+      height 7.21125rem
+    @media $breakpoint-xl
+      width 11.769375rem
+      height 11.769375rem
+    &:before
+      content ''
+      position absolute
+      opacity .32
+      width 11.22375rem
+      height 11.22375rem
+      top 50%
+      left 50%
+      mix-blend-mode hard-light
+      background radial-gradient(50% 50% at 50% 50%, #FFCEBF 35.94%, rgba(255, 206, 191, 0) 100%)
+      transform translate(-50%, -50%)
+      @media $breakpoint-medium
+        width 15.625rem
+        height 15.625rem
+      @media $breakpoint-xl
+        width 25.5rem
+        height 25.5rem
+    img
+      width 100%
+</style>
--- components/SectionTrading.vue
+++ components/SectionTrading.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="section-traiding tm-section">
-    <div class="tm-wrapper tm-content">
+    <div class="tm-wrapper tm-container-narrow">
       <div class="tm-grid-base">
         <div class="column">
-          <h2 class="tm-rf4 tm-bold tm-lh-title tm-serif title">
+          <h2 class="tm-rf4 tm-bold tm-serif title">
             Discover<br />instant<br />cross-chain<br />trading
           </h2>
-          <p class="tm-rf1 tm-lh-copy mt-7 tm-text">
-            Emeris beta connects you to the best cross-chain DeFi protocols,
-            starting with&nbsp;Gravity DEX.
+          <p class="tm-rf0 tm-rf1-l-up tm-lh-copy mt-7 tm-text">
+            Emeris beta connects you to the best cross&#8209;chain DeFi
+            protocols, starting with&nbsp;Gravity DEX.
           </p>
         </div>
 
@@ -16,30 +16,30 @@
           <ul class="list">
             <li class="list-item">
               <div class="list-info">
-                <div class="tm-rf1 tm-bold tm-lh-title tm-text">
+                <div class="tm-rf0 tm-rf1-l-up tm-bold tm-lh-title tm-text">
                   Transfer and trade assets
                 </div>
-                <div class="tm-rf0 tm-text mt-1 text-light">
+                <div class="tm-rf-1 tm-rf0-l-up tm-text mt-1 text-light">
                   Between 5 different chains for the first time
                 </div>
               </div>
             </li>
             <li class="list-item">
               <div class="list-info">
-                <div class="tm-rf1 tm-bold tm-lh-title tm-text">
+                <div class="tm-rf0 tm-rf1-l-up tm-bold tm-lh-title tm-text">
                   Access a decentralized exchange
                 </div>
-                <div class="tm-rf0 tm-text mt-1 text-light">
+                <div class="tm-rf-1 tm-rf0-l-up tm-text mt-1 text-light">
                   With a trusted, stable and audited DEX protocol
                 </div>
               </div>
             </li>
             <li class="list-item">
               <div class="list-info">
-                <div class="tm-rf1 tm-bold tm-lh-title tm-text">
+                <div class="tm-rf0 tm-rf1-l-up tm-bold tm-lh-title tm-text">
                   Earn competitive yield
                 </div>
-                <div class="tm-rf0 tm-text mt-1 text-light">
+                <div class="tm-rf-1 tm-rf0-l-up tm-text mt-1 text-light">
                   By participating in liquidity pools
                 </div>
               </div>
@@ -59,18 +59,27 @@ export default {}
 .column
   grid-column span 4
   @media $breakpoint-xl
-    grid-column span 6
+    grid-column span 5
+    &:nth-child(2n)
+      grid-column 7 / span 5
+
+.title
+  line-height 1.3
+  @media $breakpoint-medium
+    line-height 1.2
 
 .list
   list-style none
   padding-left 0
-  margin-top var(--spacing-10)
+  margin-top var(--spacing-11)
   @media $breakpoint-medium
     margin-top 0
   &-item
     position relative
     display flex
-    margin-top var(--spacing-6)
+    margin-top var(--spacing-8)
+    @media $breakpoint-xl
+      margin-top var(--spacing-6)
     &:first-child
       margin-top 0
     &:before
@@ -82,6 +91,8 @@ export default {}
       margin-top var(--spacing-1)
       clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)
       background: conic-gradient(from 180deg at 50% 50%, #CEA851 -41.21deg, #FFF1C3 0.05deg, #9B7C3A 74.45deg, #FFF0CA 115.85deg, #D3AD5F 143.94deg, #FFECC4 176.48deg, #997736 232.74deg, #FFF2C0 284.83deg, #CEA851 318.79deg, #FFF1C3 360.05deg)
+      @media $breakpoint-xl
+        margin-left -1rem
   &-info
     margin-left var(--spacing-6)
 
